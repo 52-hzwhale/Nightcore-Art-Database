4697dee8fa3b9da05b9458bb03f364787c664d17
corroscience <spacewhalesbyte@gmail.com>
2024-04-02 10:31:11 -0400

    Add more robust sorting option
    - User can now sort entries by parameter
    - User can now sort entries in ascending or descending order

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -2,8 +2,8 @@ class ArticlesController < ApplicationController
   def index
     @q = Article.ransack(params[:q])
     @per_page = params[:per_page] || Article.per_page || 2
-    #@articles = @q.result(distinct: true).page(params[:page]).order("title ASC").paginate(page: params[:page], per_page: 2)
-    @articles = @q.result(distinct: true).page(params[:page]).order("uploadDate ASC").paginate(page: params[:page], :per_page => @per_page)
+    #@articles = @q.result(distinct: true).page(params[:page]).order("uploadDate ASC").paginate(page: params[:page], :per_page => @per_page)
+    @articles = @q.result(distinct: true).page(params[:page]).order("#{params[:sort_by]} #{params[:sort_order]}").paginate(page: params[:page], :per_page => @per_page)
   end
 
   def show
